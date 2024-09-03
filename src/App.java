2642164d04901944f28e1eb336df4998031c5477
Herimanjaka Razakasoa <geit.herimanjaka@gmail.com>
2024-09-03 10:11:30 +0300

    add test in Main

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import com.finance.service.TransactionService;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        String fileName = "transactions.csv";
+        TransactionService service = new TransactionService(fileName);
+        service.getLastTransaction().ifPresent(System.out::println);
     }
 }
